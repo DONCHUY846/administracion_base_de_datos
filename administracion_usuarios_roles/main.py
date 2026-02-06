7d4a76660a720d4ba9930a6e82fcd03b9ce8ee16
jesusavina-commits <jesus.avina@roii.tech>
2026-02-06 08:50:33 -0600

    feat(functions): add main function, add conection function

--- administracion_usuarios_roles/main.py
+++ administracion_usuarios_roles/main.py
@@ -1,15 +1,46 @@
 import psycopg2
 import os
 
-try:
-    connection = psycopg2.connect(
-    host=os.getenv('HOST'),
-    user=os.getenv('USER'),
-    database=os.getenv('DB_NAME'),
-    port=os.getenv('PORT')
+def get_connection():
+   # returns the database connection
+    return psycopg2.connect(
+        host=os.getenv('HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        database=os.getenv('DB_NAME'),
+        port=os.getenv('PORT')
     )
 
-    cursor = connection.cursor()
-    print("Database connection successful")
-except Exception as e:
-    print("Error connecting to the database:", e)
+def show_databases(connection):
+    # shows the list of databases
+    cursor = None
+    try:
+        cursor = connection.cursor()
+        cursor.execute("SELECT datname FROM pg_database WHERE datistemplate = false;")
+        databases = cursor.fetchall()
+        
+        print("-- Bases de Datos ---")
+        for db in databases:
+            print(f"-> {db[0]}")
+            
+    except Exception as e:
+        print(f"Error al obtener bases de datos: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+
+def main():
+    connection = None
+    try:
+        connection = get_connection()
+        print("Conexión exitosa a PostgreSQL.")
+        show_databases(connection)
+    except Exception as e:
+        print(f"Error de conexión: {e}")
+    finally:
+        if connection:
+            connection.close()
+            print("Conexión cerrada.")
+
+if __name__ == "__main__":
+    main()
